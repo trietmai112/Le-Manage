1a599ed2a987bc37398931c95b6a4a3e5b556c7e
MinhTriet <trietmai112@gmail.com>
2017-10-11 22:34:43 +0700

    Merge branch 'Development' of https://github.com/trietmai112/MTV_LeaveManagement into Development
    # Conflicts:
    #	mtv_management_leave.Tests/LeaveTest/InOutTest.cs

---
+++ mtv_management_leave/Controllers/MasterDayOffCompanyController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using mtv_management_leave.Lib.Extendsions;
+using mtv_management_leave.Lib.Repository;
+using mtv_management_leave.Models.RegisterLeave;
+using mtv_management_leave.Models.Response;
+using mtv_management_leave.Models.Request;
+using System.Linq;
+using AutoMapper;
+using mtv_management_leave.Models.Entity;
+using mtv_management_leave.Models.Register;
+
+namespace mtv_management_leave.Controllers
+{
+    public class MasterDayOffCompanyController : Controller
+    {
+        private DayOffCompanyBase _dayOffCompanyBase;
+
+        public MasterDayOffCompanyController(DayOffCompanyBase dayOffCompanyBase)
+        {
+            _dayOffCompanyBase = dayOffCompanyBase;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ToList(RequestMasterDayOff modelRequest)
+        {
+            DateTime dateStart = modelRequest.DateStart ?? new DateTime(2000, 1, 1);
+            DateTime dateEnd = modelRequest.DateEnd ?? new DateTime(2100, 1, 1);
+
+
+            var resultApi = _dayOffCompanyBase.GetLeaveDayCompany(dateStart, dateEnd).Select(m => new ResponseMasterDayOff() { DateLeave = m.Date, Reason = m.Description }).ToList();
+
+
+            return Json(new BootGridReponse<ResponseMasterDayOff>
+            {
+                current = 1,
+                total = resultApi.Count,
+                rowCount = 20,
+                rows = resultApi
+            });
+        }
+
+        public PartialViewResult RegisterDayOffCompany()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult RegisterDayOffCompany(RegisterMasterDayOff registerDayOffCompany)
+        {
+            MasterLeaveDayCompany registerDayOff = new MasterLeaveDayCompany();
+            registerDayOff.Date = registerDayOffCompany.Date;
+            registerDayOff.Description = registerDayOffCompany.Description;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dayOffCompanyBase.SaveLeaveDayCompany(registerDayOff);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("Error", ex.Message);
+                }
+            }
+            return PartialView(registerDayOffCompany);
+        }
+    }
+}
---
+++ mtv_management_leave/Models/Response/ResponseMasterDayOff.cs
@@ -0,0 +1,14 @@
+﻿using mtv_management_leave.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mtv_management_leave.Models.Response
+{
+    public class ResponseMasterDayOff
+    {
+        public DateTime DateLeave{ get; set; }
+        public string Reason{ get; set; }
+    }
+}
