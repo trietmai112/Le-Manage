047c573e89258a6b35dfee436ad04dd43c72095c
TrungNHM <trungnhm@mytrax.co.jp>
2017-10-11 17:40:00 +0700

    no message

---
+++ mtv_management_leave/Controllers/MasterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace mtv_management_leave.Controllers
+{
+    public class MasterController:Controller
+    {
+        public PartialViewResult LeftSidebar()
+        {
+            return PartialView("_LeftSidebar", model: (User.IsInRole("Admin") || User.IsInRole("Super admin") ? "admin" : "user"));
+        }
+    }
+}
