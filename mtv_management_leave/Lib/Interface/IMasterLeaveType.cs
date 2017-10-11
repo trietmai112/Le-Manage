3735ce755ae3ac6ed6e1028ba7ffc8204a9744e3
mmtriet <mmtriet@mytrax.co.jp>
2017-10-11 15:15:50 +0700

    Push leave type api

---
+++ mtv_management_leave/Lib/Interface/IMasterLeaveType.cs
@@ -0,0 +1,16 @@
+﻿using mtv_management_leave.Models;
+using mtv_management_leave.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtv_management_leave.Lib.Interface
+{
+    interface IMasterLeaveType
+    {
+        List<RepoLeaveType> GetLeaveType();
+        List<RepoLeaveType> GetLeaveType(int id);
+    }
+}
---
+++ mtv_management_leave/Models/RepoLeaveType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace mtv_management_leave.Models
+{
+    public class RepoLeaveType
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+
+    }
+}
