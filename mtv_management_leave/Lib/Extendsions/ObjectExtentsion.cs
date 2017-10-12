b41716d5bd833a9cd2bd22273383170a37c21c88
TrungNHM <trungnhm@mytrax.co.jp>
2017-10-12 14:10:00 +0700

    commit fix POST data from client to server, fix ModelBinder of DateTime type and DateTime? Type

--- mtv_management_leave/Lib/Extendsions/ObjectExtentsion.cs
+++ mtv_management_leave/Lib/Extendsions/ObjectExtentsion.cs
@@ -4,17 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.WebPages.Html;
 
 namespace mtv_management_leave.Lib.Extendsions
 {
     public static class ObjectExtentsion
     {
-        public static Dictionary<string, object> vGetDictionary(this object model) 
+        public static IDictionary<string, object> vGetDictionary(this object model) 
         {
-            if (model == null) return new Dictionary<string, object>();
-            var properties = model.GetType().GetProperties();
+            if (model == null) throw new NullReferenceException();
+            if (model is Type)
+            {
+                return vGetDictionary((Type)model, null);
+            }
+            else
+            {
+                var type = model.GetType();
+                return vGetDictionary(type, model);
+            }
+        }
+
+        private static IDictionary<string, object> vGetDictionary(Type type, object model)
+        {            
+            var properties = type.GetProperties();
+            if (model == null) model = Activator.CreateInstance(type);
             var dic = new Dictionary<string, object>();
-            foreach(var prop in properties)
+            foreach (var prop in properties)
             {
                 dic.Add(prop.Name, prop.GetValue(model));
             }
