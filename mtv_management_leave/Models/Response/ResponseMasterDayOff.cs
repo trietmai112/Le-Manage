65cfa8f8348dd8ec97d22939709bdc97835f3972
mmtriet <mmtriet@mytrax.co.jp>
2017-10-18 11:37:35 +0700

    layout

--- mtv_management_leave/Models/Response/ResponseMasterDayOff.cs
+++ mtv_management_leave/Models/Response/ResponseMasterDayOff.cs
@@ -1,14 +1,11 @@
-﻿using mtv_management_leave.Lib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System;
 
 namespace mtv_management_leave.Models.Response
 {
     public class ResponseMasterDayOff
     {
-        public DateTime DateLeave{ get; set; }
-        public string Reason{ get; set; }
+        public int Id { get; set; }
+        public DateTime DateLeave { get; set; }
+        public string Reason { get; set; }
     }
 }
